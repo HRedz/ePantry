0063591416f1f568346c934bc84da4608664abc5
d-arshad <arshad.daniyah@gmail.com>
2024-04-01 17:33:27 -0500

    Fixed styling

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,9 +5,14 @@ const Navbar = () => {
    return (
       <header>
          <div className="container">
+            <nav1>
             <Link to="/">
-               <h1>ePantry</h1>
+               <title1><h1>ePantry</h1></title1>
             </Link>
+
+            <a href="/">Log In</a>
+            <a href="/">Sign Up</a>
+            </nav1>
          </div>
       </header>
    )
