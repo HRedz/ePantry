506d5310461cdf909cf4f1023239587cfece43a0
fredtrinh <fredtrinh5@gmail.com>
2024-04-15 20:27:20 -0500

    3 profile types available

--- frontend/src/pages/UserProfile.js
+++ frontend/src/pages/UserProfile.js
@@ -4,23 +4,89 @@ const UserProfile = () => {
 
     const user = JSON.parse(localStorage.getItem('user'));
 
+    const separateProfiles = () => {
+      switch (user.type) {
+        
+        
+        case 'individual':
+          return (
+            <div style={{ textAlign: 'center', marginTop: '50px' }}>
+          <img
+            src="https://servicios.intec.santafe-conicet.gov.ar/grupos/iayb/wp-content/uploads/2015/04/Generic-avatar-2.png"
+            alt="Profile"
+            style={{ borderRadius: '50%' }}
+          />
+          <h1>Individual: {user.name}</h1>
+          <p>Email: {user.email}</p>
+          <button onClick={() => window.location.href = '/donationhistory'}>
+            View Donation History
+          </button>
+          <button onClick={() => window.location.href = '/permissions'}>
+            View Pending Permissions
+          </button>
+        </div>
+        );
+
+        
+        
+        case 'company':
+          return (
+            <div style={{ textAlign: 'center', marginTop: '50px' }}>
+          <img
+            src="https://servicios.intec.santafe-conicet.gov.ar/grupos/iayb/wp-content/uploads/2015/04/Generic-avatar-2.png"
+            alt="Profile"
+            style={{ borderRadius: '50%' }}
+          />
+          <h1>Company: {user.name}</h1>
+          <p>Email: {user.email}</p>
+          <button onClick={() => window.location.href = '/donationhistory'}>
+            View Donation History
+          </button>
+          <button onClick={() => window.location.href = '/permissions'}>
+            View Pending Permissions
+          </button>
+        </div>
+        );
+
+        
+        
+        case 'organization':
+          return (
+            <div style={{ textAlign: 'center', marginTop: '50px' }}>
+          <img
+            src="https://servicios.intec.santafe-conicet.gov.ar/grupos/iayb/wp-content/uploads/2015/04/Generic-avatar-2.png"
+            alt="Profile"
+            style={{ borderRadius: '50%' }}
+          />
+          <h1>Organization: {user.name}</h1>
+          <p>Email: {user.email}</p>
+          <button onClick={() => window.location.href = '/donationhistory'}>
+            View Donation History
+          </button>
+          <button onClick={() => window.location.href = '/'}>
+            Approve/Deny Donation Applications
+          </button>
+          <button onClick={() => window.location.href = '/'}>
+            View Grant Status
+          </button>
+          <button onClick={() => window.location.href = '/'}>
+            Donations Received
+          </button>
+        </div>
+        );
+
+
+        default:
+          return <p>Profile Error</p>
+      }
+    };
+
     return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <img
-        src="https://servicios.intec.santafe-conicet.gov.ar/grupos/iayb/wp-content/uploads/2015/04/Generic-avatar-2.png"
-        alt="Profile"
-        style={{ borderRadius: '50%' }}
-      />
-      <h1>{user.name}</h1>
-      <p>Email: {user.email}</p>
-      <button onClick={() => window.location.href = '/donationhistory'}>
-        View Donation History
-      </button>
-      <button onClick={() => window.location.href = '/permissions'}>
-        View Pending Permissions
-      </button>
-    </div>
+      <div>
+        {separateProfiles()}
+      </div>
     );
+
 }
 
 export default UserProfile;
